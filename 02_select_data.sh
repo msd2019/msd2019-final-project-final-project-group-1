458e69aa2ef2fd81e700cb33db71e5891e56e393
Forrest Hofmann <f.hofmann29@gmail.com>
2019-05-02 17:15:57 -0400

    Add world series data

--- 02_select_data.sh
+++ 02_select_data.sh
@@ -4,4 +4,4 @@
 cat ./baseballdatabank-2019.2/core/Salaries.csv | cut -d ',' -f 1,2,4,5 > salaries.csv
 
 # select fields for team information
-cat ./baseballdatabank-2019.2/core/Teams.csv | cut -d ',' -f 1,3,7,9,10 > teams.csv
+cat ./baseballdatabank-2019.2/core/Teams.csv | cut -d ',' -f 1,3,7,9,10,14 > teams.csv
